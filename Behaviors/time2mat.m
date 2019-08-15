659711de8e1e692b474599f578bb9071c4dd27c3
Stephen Zhang <x.stephen.zhang@gmail.com>
2019-08-15 15:20:41 -0400

    Create time2mat.m

---
+++ Behaviors/time2mat.m
@@ -0,0 +1,54 @@
+function time2mat(A)
+% time2mat convert scoring data to behavioral matrix
+% time2mat(A)
+
+%% Initialization
+% Default path
+defaultpath = '\\anastasia\data\photometry';
+
+% Recording frame rate
+cam_save_fps = 30;
+
+% Camera pulse channel
+cam_pulse_ch = 4;
+
+%% Processing
+
+% Find A matrix
+if isempty(A)
+    disp('A-matrix cannot be empty.')
+else
+    % Got a working A-matrix
+    % Work out outputpath
+    [filename, filepath] = uigetfile(fullfile(defaultpath , '*nidaq.mat'));
+    filename_output = [filename(1:end-4), '_A.mat'];
+    load(fullfile(filepath, filename), 'data', 'Fs');
+
+    % Find camera frame rate
+    [p_ch1, freq_ch1] = ft2(data(cam_pulse_ch, :), Fs, false);
+    [PKS,LOCS]= findpeaks(p_ch1);
+    [~, id] = max(PKS);
+    cam_pulse_fps = freq_ch1(LOCS(id));
+
+    % If more than one row of behvioral scoring
+    if size(A,2) > 1
+        A = A';
+        A = A(:);
+    end
+
+    A = reshape(A, 3, [])';
+    A = A(A(:,2) > 0, :);
+    
+    % Calculate delay
+    delay =  find(data(cam_pulse_ch,:) > 0.5, 1) / Fs;
+    
+    % Get aligned behavioral matrix ready
+    B = A;
+    
+    % Align behavioral matrix
+    B(:,2:3) = A(:,2:3) * cam_save_fps / cam_pulse_fps + delay/60;
+    
+    % Save
+    save(fullfile(filepath, filename_output), 'A', 'B', 'cam_pulse_fps');
+end
+end
