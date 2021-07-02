e31f8bd9710a9752524974c5d25811223d247b28
paulazoo <paulazhu@college.harvard.edu>
2021-07-02 16:48:31 -0400

    dont use duration

--- Behaviors/exceltime3.m
+++ Behaviors/exceltime3.m
@@ -11,8 +11,19 @@ definput = {''};
 inputdata = inputdlg(prompt, dlgtitle, dims, definput);
 
 data = textscan(inputdata{:},'%f%s%s');
-key=[data{:, 1}];
-start_time=minutes(duration(data{:, 2}, 'InputFormat', 'mm:ss'));
-end_time=minutes(duration(data{:, 3}, 'InputFormat', 'mm:ss'));
+key =[data{:, 1}];
+
+% manually parse -_-
+pre_start_time = cellfun(@(x) strsplit(x, ':'), data{:, 2}, 'UniformOutput', false);
+pre_start_time = vertcat(pre_start_time{:});
+pre_start_time = cell2mat(cellfun(@(x) str2num(x), pre_start_time, 'UniformOutput', false));
+start_time = pre_start_time(:, 1) + (pre_start_time(:, 2)/60);
+
+% manually parse -_-
+pre_end_time = cellfun(@(x) strsplit(x, ':'), data{:, 3}, 'UniformOutput', false);
+pre_end_time = vertcat(pre_end_time{:});
+pre_end_time = cell2mat(cellfun(@(x) str2num(x), pre_end_time, 'UniformOutput', false));
+end_time = pre_end_time(:, 1) + (pre_end_time(:, 2)/60);
+
 outputmat = [key start_time end_time];
 end
