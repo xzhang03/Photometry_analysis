11d7968474915c8d82cf318a0cf8140d06843a42
Stephen Zhang <x.stephen.zhang@gmail.com>
2021-07-02 17:23:09 -0400

    Update exceltime3.m

--- Behaviors/exceltime3.m
+++ Behaviors/exceltime3.m
@@ -18,13 +18,13 @@ key =[data{:, 1}];
 % start time for each behavior
 pre_start_time = cellfun(@(x) strsplit(x, ':'), data{:, 2}, 'UniformOutput', false);
 pre_start_time = vertcat(pre_start_time{:});
-pre_start_time = cell2mat(cellfun(@(x) str2num(x), pre_start_time, 'UniformOutput', false));
+pre_start_time = cell2mat(cellfun(@(x) str2double(x), pre_start_time, 'UniformOutput', false));
 start_time = pre_start_time(:, 1) + (pre_start_time(:, 2)/60);
 
 % end time for each behavior
 pre_end_time = cellfun(@(x) strsplit(x, ':'), data{:, 3}, 'UniformOutput', false);
 pre_end_time = vertcat(pre_end_time{:});
-pre_end_time = cell2mat(cellfun(@(x) str2num(x), pre_end_time, 'UniformOutput', false));
+pre_end_time = cell2mat(cellfun(@(x) str2double(x), pre_end_time, 'UniformOutput', false));
 end_time = pre_end_time(:, 1) + (pre_end_time(:, 2)/60);
 
 % combine into output matrix
