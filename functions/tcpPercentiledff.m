34a4fd95243e461fbfebb44b1344699e70ca05d9
Stephen Zhang <x.stephen.zhang@gmail.com>
2022-02-25 11:35:11 -0500

    Update tcpPercentiledff.m

--- functions/tcpPercentiledff.m
+++ functions/tcpPercentiledff.m
@@ -2,7 +2,7 @@ function out = tcpPercentiledff(vec, fps, time_window, percentile)
 % tcpPercentiledff Return a dff with the 10th percentile subtracted across
 % a default 32-second window. Input data cannot be negative
 % out = tcpPercentiledff(vec, fps, time_window, percentile, offset)
-
+% Now deals with nans better
 
 % Default values from Rohan and Christian
 % time_window is moving window of X seconds - 
@@ -13,12 +13,14 @@ if nargin < 4, percentile = 10; end
 % Get the number of points
 nframes = length(vec);
 
-
-
 %% Now calculate dFF using axon method
 time_window_frame = round(time_window*fps);
 
-f0 = nan(nframes, 1);
+if size(vec,1) == 1
+    f0 = nan(1, nframes);
+else
+    f0 = nan(nframes, 1);
+end
 for i = 1:nframes
     if i <= time_window_frame
         frames = vec(1:time_window_frame);
@@ -28,6 +30,9 @@ for i = 1:nframes
         f0(i) = prctile(frames, percentile);
     end
 end
+
+% debug
+% plot([vec;f0]');
 
 out = (vec - f0)./f0; 
 end
